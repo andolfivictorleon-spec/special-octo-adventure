34a9698c32d013fae2502a91ed95955c6d77df49
andolfivictorleon-spec <andolfi.victor.leon@gmail.com>
2026-08-01 16:49:55 +0200

    Add buildings data structure with initial values

---
+++ data.js
@@ -0,0 +1,54 @@
+const buildings = {
+
+    biblioteca: {
+
+        nome:"Biblioteca",
+
+        emoji:"📚",
+
+        livello:0,
+
+        punteggio:0
+
+    },
+
+
+    municipio: {
+
+        nome:"Municipio",
+
+        emoji:"🏛️",
+
+        livello:0,
+
+        punteggio:0
+
+    },
+
+
+    laboratorio: {
+
+        nome:"Laboratorio",
+
+        emoji:"🔬",
+
+        livello:0,
+
+        punteggio:0
+
+    },
+
+
+    museo: {
+
+        nome:"Museo d'Arte",
+
+        emoji:"🎨",
+
+        livello:0,
+
+        punteggio:0
+
+    }
+
+};
